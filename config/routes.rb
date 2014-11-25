a8a3c95dbf73cf495a2f55b2de2774a53e2988b0
David Shack <lol@gmail.com>
2014-11-25 09:23:56 -0600

    Added Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :categories
+
   resources :products, only: [:index]
   resource :cart, only: [:show]
   resources :order_items, only: [:create, :update, :destroy]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141117131402) do
+ActiveRecord::Schema.define(version: 20141125152254) do
 
   create_table "Products", force: true do |t|
     t.string   "sku"
@@ -57,6 +57,12 @@ ActiveRecord::Schema.define(version: 20141117131402) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "order_items", force: true do |t|
     t.integer  "product_id"
